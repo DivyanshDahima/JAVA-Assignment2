c1ec4754d9c06c94bb981fbc59fdac70665ca371
DivyanshDahima <divyanshdahima7@gmail.com>
2024-11-25 00:44:38 +0530

    Add files via upload

---
+++ Q7.java
@@ -0,0 +1,31 @@
+class Animal {
+    public void sound() {
+        System.out.println("Animals make sounds.");
+    }
+}
+
+class Dog extends Animal {
+    @Override
+    public void sound() {
+        System.out.println("Dogs bark.");
+    }
+}
+
+class Cat extends Animal {
+    @Override
+    public void sound() {
+        System.out.println("Cats meow.");
+    }
+}
+
+public class Q7 {
+    public static void main(String[] args) {
+        Animal myAnimal = new Animal();
+        Animal myDog = new Dog();
+        Animal myCat = new Cat();
+
+        myAnimal.sound();
+        myDog.sound();
+        myCat.sound();
+    }
+}
